79dcff14b70fc0492471fc3dceb340556ccbced9
dOJer113 <aleksandrroznov484@gmail.com>
2023-07-03 20:04:14 +0300

    Last

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,40 @@
-import java.io.File;
-import java.util.Arrays;
+import com.google.gson.GsonBuilder;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
+    public static final String Dir = "pdfs";
     public static void main(String[] args) throws Exception {
-        BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
-        System.out.println(engine.search("бизнес"));
+        BooleanSearchEngine engine = new BooleanSearchEngine(new File(Dir));
 
-        // здесь создайте сервер, который отвечал бы на нужные запросы
-        // слушать он должен порт 8989
-        // отвечать на запросы /{word} -> возвращённое значение метода search(word) в JSON-формате
+        try (ServerSocket serverSocket = new ServerSocket(8989)) {
+
+            System.out.println("Сервер запущен");
+
+            while (true) {
+                try (
+                        Socket socket = serverSocket.accept();
+                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        PrintWriter out = new PrintWriter(socket.getOutputStream());
+                ) {
+                    String str = in.readLine();
+                    System.out.println(str);
+
+                    GsonBuilder builder = new GsonBuilder();
+                    out.write(builder.setPrettyPrinting().create().toJson(engine.search(str)));
+                    System.out.println(builder.setPrettyPrinting().create().toJson(engine.search(str)));
+
+
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Не получилось запустить сервер ");
+            e.printStackTrace();
+        }
+
     }
 }
--- src/main/java/PageEntry.java
+++ src/main/java/PageEntry.java
@@ -1,7 +1,32 @@
 public class PageEntry implements Comparable<PageEntry> {
-    private final String pdfName;
+    private final String namePdf;
     private final int page;
     private final int count;
 
-    // ???
+    public PageEntry(String namePdf, int page, int count) {
+        this.namePdf = namePdf;
+        this.page = page;
+        this.count = count;
+    }
+
+
+
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public int compareTo(PageEntry o) {
+        return Integer.compare(o.getCount(), this.getCount());
+    }
+
+    @Override
+    public String toString() {
+        return "PageEntry{" +
+                "\n\t namePdf='" + namePdf +
+                "\n\t page=" + page +
+                "\n\t count=" + count +
+                "\n\t" + '}';
+    }
+
 }
